d80e007b234511e6bfc81cbac1b23165532bd0cc
danielsky <karma0rg4u@gmail.com>
2019-03-11 00:19:58 +0000

    Fixed typo in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
