ae346c928090ff308230bf1b7b4b3063bed4ffb2
danielsky <karma0rg4u@gmail.com>
2019-03-14 14:38:52 +0000

    Add html pages to the project and to nav links

--- app.py
+++ app.py
@@ -12,9 +12,28 @@ app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_categories')
-def get_categories():
-    return render_template('categories.html', categories=mongo.db.categories.find())
+def index():
+    return render_template('index.html', categories=mongo.db.categories.find())
+
+@app.route('/locations')
+def locations():
+    return render_template('locations.html', locations=mongo.db.locations.find())
+
+@app.route('/search')
+def search():
+    return render_template('search.html', locations=mongo.db.locations.find())
+
+@app.route('/add_spot')
+def add_spot():
+    return render_template('addSpot.html', locations=mongo.db.locations.find())
+
+@app.route('/about')
+def about():
+    return render_template('about.html', locations=mongo.db.locations.find())
+
+@app.route('/register')
+def register():
+    return render_template('register.html', locations=mongo.db.locations.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
